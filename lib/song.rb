6fbdbe91453caa7f85b730d5e9dd9722a4016e31
Sajid Anjum <sajid1760@gmail.com>
2020-03-14 02:06:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,13 +48,15 @@ end
 
 end
 
-#lucifer = Song.new("Lucifer", "Jay-Z", "rap")
-#ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap") 
-#hit_me = Song.new("hit me baby one more time", "Brittany Spears", "pop") 
+lucifer = Song.new("Lucifer", "Jay-Z", "rap")
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap") 
+hit_me = Song.new("hit me baby one more time", "Brittany Spears", "pop") 
 
-#puts lucifer.name   
-#puts lucifer.artist_count
-#puts lucifer.genre_count
-##puts lucifer.genre
-#puts lucifer.artist
+puts lucifer.name   
+puts lucifer.artist_count
+puts lucifer.genre_count
+puts lucifer.genre
+puts lucifer.artist
+puts lucifer.artists
+puts lucifer.genres
     
