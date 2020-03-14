2365221df84aec162ad65ba309adeebf86673852
Sajid Anjum <sajid1760@gmail.com>
2020-03-14 01:49:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,25 @@
 class song 
   
-  @@classsong = {}
+  @@count = 0 
+  @@genres = []
+  @@artists = []
   
   attr_writer :name, :artist, :genre
   
-  def initialize(name,artist,genre)
+def initialize(name,artist,genre)
     
     @name = name
     @artist = artist
     @genre = genre
-    if !@@classsong.include?(genre) then
-      @@classsong[genre] = {} end
-    if !@@classsong[genre].include?(arist) then
-      @@classsong[genre][artist] = [] end
-    @@classsong[genre][artist] << name
-    
-  end
+    @@count += 1 
+    if !@@genres.include?(genre) then @@genres << genre end
+    if !@@artists.include?(genre) then @@artists << genre end
+end
+
+def count
+  @@count
+end
+
+
     
     
