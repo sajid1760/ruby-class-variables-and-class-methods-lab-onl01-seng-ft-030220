0e8856a204194464ed12914e78bee95a81f97413
Sajid Anjum <sajid1760@gmail.com>
2020-03-14 01:54:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,14 +14,31 @@ def initialize(name,artist,genre)
     @artist = artist
     @genre = genre
     @@count += 1 
-    if !@@genres.include?(genre) then @@genres << genre end
-    if !@@artists.include?(genre) then @@artists << genre end
+    if !@@genres.include?(genre) then 
+       @@genres << genre 
+       @@genrecount[genre] = 0
+       end
+       @@genrecount[genre] += 1
+    if !@@artists.include?(artist) then 
+        @@artists << artist 
+        @@artistcount[artist] = 0
+        end
+        @@artistcount[artist] += 1
 end
 
 def count
   @@count
 end
 
+def genre_count
+  @@genrecount
+end
+
+def artistcount
+  @@artistcount
+end
+
+end
 
 
 
